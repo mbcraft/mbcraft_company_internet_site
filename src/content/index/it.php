4baa5bced1d6be7f4cb7568686b0c821a77fa105
Marco Bagnaresi <info@mbcraft.it>
2024-02-17 15:47:14 +0100

    Rimozione contenuti poco simpatici

--- src/content/index/it.php
+++ src/content/index/it.php
@@ -26,7 +26,7 @@ ob_start();
 </div>
     <br />
     <div align="center">
-    <h4>Ultimo aggiornamento : 16/02/2024</h4>
+    <h4>Ultimo aggiornamento : 17/02/2024</h4>
     </div>
     <br />
     <br />
