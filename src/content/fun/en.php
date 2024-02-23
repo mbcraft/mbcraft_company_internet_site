3c856c3ee39a0fc73b35dd0080cd75f3f0041a68
Marco Bagnaresi <info@mbcraft.it>
2024-02-23 11:03:20 +0100

    Added another meme

--- src/content/fun/en.php
+++ src/content/fun/en.php
@@ -41,6 +41,7 @@ ob_start();
 		<li><a href="/pages/meme_festivita_eng.php">24</a> (EVEN FOR THIS ONE I TAKE NO RESPONSIBILITY ...)</li>
 		<li><a href="/pages/meme_password_changed_eng.php">25</a></li>
 		<li><a href="/pages/meme_smartphone_evolution.php">26</a></li>
+		<li><a href="/pages/meme_shaolin_soccer_eng.php">27</a></li>
 	</ul>
 </span>
 </h2>
