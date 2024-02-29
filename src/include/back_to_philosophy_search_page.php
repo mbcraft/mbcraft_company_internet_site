05ee8c64697f926036ec02d40867307a992f3d19
Marco Bagnaresi <info@mbcraft.it>
2024-02-29 17:12:32 +0100

    Fix backgrounds of buttons

--- src/include/back_to_philosophy_search_page.php
+++ src/include/back_to_philosophy_search_page.php
@@ -2,7 +2,7 @@
     <div class='w3-container s6 m4 l3 w3-center w3-margin'>
         <div class='w3-cell-row'>
             <?php
-                echo_generic_big_page_button("document.location.href='/philosophy.php';","back_button_background.jpg","white","Torna alla ricerca","Back to search");
+                echo_generic_big_page_button("document.location.href='/philosophy.php';","back_to_search_button_background.jpg","white","Torna alla ricerca","Back to search");
             ?>
         </div>
     </div>
