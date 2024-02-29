04a44347417cb688f5fb467594cbfde6c741199b
Marco Bagnaresi <info@mbcraft.it>
2024-02-29 17:20:47 +0100

    Fix buttons that should disappear

--- src/include/begin_content_with_language_switches.php
+++ src/include/begin_content_with_language_switches.php
@@ -3,7 +3,7 @@
 <body>
     <?php
     include_once(__DIR__."/support_functions.php");
-    include_once(__DIR__."/language_switches.php");
+    if (!isset($_GET["mode"])) include_once(__DIR__."/language_switches.php");
     include_once(__DIR__."/background.php");
     ?>
     <div id="main_text" style="position:relative;z-index:50;width:100%;height:100%;">
