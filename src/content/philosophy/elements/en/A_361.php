4220610319613455171163e0f8276ba0650a97c2
Marco Bagnaresi <info@mbcraft.it>
2024-02-29 09:31:31 +0100

    Fix A361

--- src/content/philosophy/elements/en/A_361.php
+++ src/content/philosophy/elements/en/A_361.php
@@ -1,1 +1,1 @@
-A361 - For those who have prostheses or use a wheelchair, it might be a good idea to also insert a piece of crystal (e.g. quartz) into a special housing inside the prosthesis or wheelchair.
+A361 - For those who have prostheses or use a wheelchair, it might be a good idea to also insert a piece of crystal (e.g. quartz) into a special housing inside the prosthesis or wheelchair, or even use space to hold objects (e.g. a Swiss army knife).
