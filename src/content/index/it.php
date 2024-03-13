b47e51066cfb82d9b86a1b96e033fa3d4541f9ae
Marco Bagnaresi <info@mbcraft.it>
2024-03-13 17:08:50 +0100

    Some changes

--- src/content/index/it.php
+++ src/content/index/it.php
@@ -26,7 +26,7 @@ ob_start();
 </div>
     <br />
     <div align="center">
-    <h4>Ultimo aggiornamento : 12/03/2024</h4>
+    <h4>Ultimo aggiornamento : 13/03/2024</h4>
     </div>
     <br />
     <br />
