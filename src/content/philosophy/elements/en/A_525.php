a35c554a7a20b6588a270b2de8e6ab552ccb5db6
Marco Bagnaresi <info@mbcraft.it>
2024-04-26 10:19:31 +0200

    Little fixes

--- src/content/philosophy/elements/en/A_525.php
+++ src/content/philosophy/elements/en/A_525.php
@@ -1,1 +1,1 @@
-A525 - Pregnant women must be treated with great attention and respect, as they sometimes feel uncomfortable psychologically as well as physically.
+A525 - Pregnant women must be treated with great attention and respect, as they sometimes feel uncomfortable psychologically, emotionally or physically.
