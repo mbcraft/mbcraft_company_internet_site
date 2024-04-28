f1c63cf40b8b533d9890b433f487fa181178e35f
Marco Bagnaresi <info@mbcraft.it>
2024-04-28 20:04:55 +0200

    Added more elements

---
+++ src/content/philosophy/elements/it/A_528.php
@@ -0,0 +1,1 @@
+A528 - E' risaputo che l'acqua clorata non &egrave; adatta per innaffiare le piante, se possibile usate acqua filtrata, acqua piovana, acqua di pozzi o acqua minerale, in quanto dovrebbe essere meglio dell'uso dell'acqua del rubinetto non filtrata.
