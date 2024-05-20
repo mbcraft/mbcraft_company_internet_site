c12e8ea608d095adc216ee86e5f5bfa1e8be22dc
Marco Bagnaresi <info@mbcraft.it>
2024-05-20 17:42:28 +0200

    Feedback form sistemata

--- src/content/feedback_thank_you/it.php
+++ src/content/feedback_thank_you/it.php
@@ -5,11 +5,18 @@ $title_suffix = "Ringraziamento per feedback";
 $description = "Ringraziamento del feedback.";
 $keywords = "feedback, ringraziamento, MBCRAFT";
 
+$thank_you_statement = array(
+
+"Un gatto ora ti pensa ...","Un'anatra, da qualche parte, guarda le stelle ...","Un criceto ha fatto un disegno della tua faccia nella sua tana ...","Un koala ora sa che pu&ograve; contare anche su di te ...","Un panda rotola gi&ugrave; da un dirupo e infligge 3d6 danni psicologici al tuo peggior nemico ..."
+);
+
+$s = $thank_you_statement[rand(0,count($thank_you_statement)-1)];
+
 ob_start();
 ?>
 <div align="center">
 	<h1>
-	GRAZIE PER AVER INVIATO QUESTO FEEDBACK
+	GRAZIE PER AVER INVIATO QUESTO FEEDBACK : <?=$s ?>
 	</h1>
 </div>
 <?php
