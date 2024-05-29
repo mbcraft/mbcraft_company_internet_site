643f47e9adaca42e7d8ecf5381fd8bfaa3269957
Marco Bagnaresi <info@mbcraft.it>
2024-05-29 13:49:04 +0200

    Some additions

--- src/content/index/it.php
+++ src/content/index/it.php
@@ -41,7 +41,7 @@ require_once ("include/site_variables.php");
 </div>
     <br />
     <div align="center">
-    <h4>Ultimo aggiornamento : 27/05/2024</h4>
+    <h4>Ultimo aggiornamento : 29/05/2024</h4>
     </div>
     <br />
     <br />
