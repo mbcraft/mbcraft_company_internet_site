7b15a6171851e43832272fb7e6f28bb0a6dc1c8a
Marco Bagnaresi <info@mbcraft.it>
2024-07-18 15:39:10 +0200

    One more elemenet

--- src/include/philosophy_page_support_functions.php
+++ src/include/philosophy_page_support_functions.php
@@ -1,7 +1,7 @@
 <?php
 
 $range_H_A = array('H',1,13,'black');
-$range_A_A = array('A',1,614,'black');
+$range_A_A = array('A',1,617,'black');
 $range_B_A = array('B',1,18,'black');
 $range_C_A = array('C',1,9,'black');
 $range_D_A = array('D',1,4,'black');
