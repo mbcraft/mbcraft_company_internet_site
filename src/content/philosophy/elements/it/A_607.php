9a8d9c1fa549b46c16850820b4bbfb7e4d451956
Marco Bagnaresi <info@mbcraft.it>
2025-06-22 11:21:51 +0200

    Added A_632 and updated A_607

--- src/content/philosophy/elements/it/A_607.php
+++ src/content/philosophy/elements/it/A_607.php
@@ -1,10 +1,10 @@
 A_607 - Le scorie nucleari sarebbe meglio non stoccarle nei depositi cercando di dimenticarsene ma cercare di trovare altre sistemazioni migliori. Alcune idee potrebbero essere le seguenti :
 <ul>
 		<li>
-			Lasciate nel Sahara in modo che interagiscano con la sabbia creandone lievi alterazioni. Con tutto il posto disponibile anche lasciarle aperto sarebbe un'ottima soluzione, in quanto poi il vento trasporta poi la sabbia leggermente alterata fino alla foresta Amazzonica, e in quel luogo funge da fertilizzante
+			Lasciate nel Sahara in modo che interagiscano con la sabbia creandone lievi alterazioni. Con tutto il posto disponibile anche lasciarle aperto sarebbe un'ottima soluzione, in quanto poi il vento trasporta poi la sabbia leggermente alterata fino alla foresta Amazzonica, e in quel luogo funge da fertilizzante, oppure anche sepolte qualche metro sotto la sabbia del deserto in modo che rimangano lì.
 		</li>
 		<li>
-			Nella bocca di un vulcano
+			Nel cratere di un vulcano non attivo
 		</li>
 		<li>
 			Usate per creare blocchi da usare per testare nuove tipologie di reazioni chimiche, che avvengono in presenza dell'emissione di neutroni da parte delle scorie instabili.
@@ -13,4 +13,3 @@ A_607 - Le scorie nucleari sarebbe meglio non stoccarle nei depositi cercando di
 			Miscelate ad altre sostanze per creare nuove tipologie di sostanze chimiche sconosciute ma che non risultano pi&ugrave; radioattive. Per raggiungere questo scopo si potrebbero anche usare macchinari per effettuare reazioni chimiche in serie con degli automatismi, in modo da effettuare molteplici prove molto rapidamente. Oltre a questo non &egrave; nemmeno vietato usare l'AI per cercare di capire se con qualche sostanza chimica esistono delle possibilit&agrave; che non conosciamo per sfruttare queste sostanze chimiche con intelligenza, in modo che da problema diventino risorsa.
 		</li>
 </ul>
-Nota : queste informazioni sono volte unicamente ad aiutare nella gestione delle scorie. Non sono da intendere come una mia presa di posizione totalmente favorevole nei confronti dell'uso di tale tipologie di energia.
